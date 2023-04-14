6147dedae9dff6823fec3ac73a87e9a969e3f886
scsk-nakagome <nakagome@scsk.jp>
2023-04-14 16:34:15 +0900

    4/14

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/api1", api1Handler)
+	http.HandleFunc("/api2", api2Handler)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -42,6 +44,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Date %s\n", res1.Header["Date"])
 	fmt.Fprintf(w, "Content-Type %s\n", res1.Header["Content-Type"])
 	fmt.Fprintf(w, "Method %s\n", res1.Request.Method)
+
+	res2, _ := http.Get("https://go-codescanning-githubactions-cloudrun-api2-wsgwmfbvhq-uc.a.run.app")
+	// 取得したURLの内容を読み込む
+	body2, _ := io.ReadAll(res2.Body)
+	// 取得した情報は[]byteなのでstringに型変換
+	fmt.Fprintf(w, "Body %s\n", string(body2))
+	fmt.Fprintf(w, "Status %s\n", res2.StatusCode)
+	fmt.Fprintf(w, "URL %s\n", res2.Request.URL)
+	fmt.Fprintf(w, "Protocol %s\n", res2.Proto)
+	fmt.Fprintf(w, "Date %s\n", res2.Header["Date"])
+	fmt.Fprintf(w, "Content-Type %s\n", res2.Header["Content-Type"])
+	fmt.Fprintf(w, "Method %s\n", res2.Request.Method)
+}
+
+func api1Handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "bff"
+	}
+	res1, _ := http.Get("https://go-codescanning-githubactions-cloudrun-api1-wsgwmfbvhq-uc.a.run.app")
+	// 取得したURLの内容を読み込む
+	body1, _ := io.ReadAll(res1.Body)
+	// 取得した情報は[]byteなのでstringに型変換
+	fmt.Fprintf(w, "Body %s\n", string(body1))
+	fmt.Fprintf(w, "Status %s\n", res1.StatusCode)
+	fmt.Fprintf(w, "URL %s\n", res1.Request.URL)
+	fmt.Fprintf(w, "Protocol %s\n", res1.Proto)
+	fmt.Fprintf(w, "Date %s\n", res1.Header["Date"])
+	fmt.Fprintf(w, "Content-Type %s\n", res1.Header["Content-Type"])
+	fmt.Fprintf(w, "Method %s\n", res1.Request.Method)
+
+}
+
+func api2Handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "bff"
+	}
 
 	res2, _ := http.Get("https://go-codescanning-githubactions-cloudrun-api2-wsgwmfbvhq-uc.a.run.app")
 	// 取得したURLの内容を読み込む
