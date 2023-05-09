ac974854e1814451627a7f82efba9ea61a3bf286
scsk-nakagome <nakagome@scsk.jp>
2023-05-09 15:22:21 +0900

    JWT verify

--- go.mod
+++ go.mod
@@ -4,22 +4,13 @@ go 1.13
 
 require (
 	cloud.google.com/go/compute v1.19.1 // indirect
-	cloud.google.com/go/workflows v1.10.0 // indirect
-	github.com/bytedance/sonic v1.8.7 // indirect
-	github.com/gin-gonic/gin v1.9.0 // indirect
-	github.com/go-playground/validator/v10 v10.12.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	cloud.google.com/go/workflows v1.10.0
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/s2a-go v0.1.1 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/leodido/go-urn v1.2.3 // indirect
-	github.com/mattn/go-isatty v0.0.18 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
-	github.com/ugorji/go/codec v1.2.11 // indirect
-	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
-	google.golang.org/api v0.118.0 // indirect
+	google.golang.org/api v0.118.0
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -10,7 +11,11 @@ import (
 
 	executions "cloud.google.com/go/workflows/executions/apiv1"
 	executionspb "cloud.google.com/go/workflows/executions/apiv1/executionspb"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
 	"google.golang.org/api/idtoken"
+
+	"encoding/json"
 )
 
 const Api1Url = "https://fika-api1-nakagome-wsgwmfbvhq-uc.a.run.app"
@@ -20,7 +25,27 @@ const ProjectId = "kaigofika-poc01"
 const Location = "us-central1"
 const workflowName = "fs-workflow-nakagome"
 
+const Audience = "https://fs-apigw-bff-nakagome-bi5axj14.uc.gateway.dev/"
+const DomainName = "dev-kjqwuq76z8suldgw.us.auth0.com"
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
 func main() {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: verifyToken,
+		SigningMethod:       jwt.SigningMethodRS256,
+	})
+
 	http.HandleFunc("/workflow", workflowHandler)
 	http.HandleFunc("/api2", api2Handler)
 
@@ -31,6 +56,67 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func verifyToken(token *jwt.Token) (interface{}, error) {
+	log.Println("verifyToken entering")
+	log.Println("token")
+	log.Println(token)
+	// Verify 'aud' claim
+	aud := Audience
+	checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+	if !checkAud {
+		fmt.Printf("Invalid audience.\n")
+		return token, errors.New("Invalid audience.")
+	}
+	// Verify 'iss' claim
+	iss := "https://" + DomainName + "/"
+	checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+	if !checkIss {
+		return token, errors.New("Invalid isssuer.")
+	}
+
+	cert, err := getPemCert(token)
+	if err != nil {
+		panic(err.Error())
+	}
+	result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+	log.Println("verifyToken exiting")
+	return result, nil
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	log.Println("getPemCert entering")
+	// Auth0の公開鍵を取得
+	cert := ""
+	resp, err := http.Get("https://" + DomainName + "/.well-known/jwks.json")
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+	log.Println("resp %v", resp.Body)
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	log.Println("getPemCert exiting")
+	return cert, nil
 }
 
 // BFF → workflow → api2 呼び出し
@@ -68,6 +154,12 @@ func workflowHandler(w http.ResponseWriter, r *http.Request) {
 func api2Handler(w http.ResponseWriter, r *http.Request) {
 	// Auth0の認証情報を取り出す
 	auth0Token := r.Header.Get("X-Forwarded-Authorization")
+
+	// 認証情報の検証
+	resp, err := verifyToken(auth0Token)
+	log.Println(resp)
+
+	// api2へのAuthorization Headerの引き渡し
 	ctx := context.Background()
 	// TEST - contextがhttpでうまくいかない
 	// ctx := context.WithValue(context.Background(), "auth0-token", auth0Token)
