219ab099e6182195996d2aca958b99f93f5cedc8
scsk-nakagome <nakagome@scsk.jp>
2023-05-12 17:00:52 +0900

    5/12

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,11 +1,11 @@
 package middleware
 
 import (
-	"context"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
+
+	// "strings"
 	"time"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
@@ -13,28 +13,31 @@ import (
 	"github.com/auth0/go-jwt-middleware/v2/validator"
 )
 
-// CustomClaims contains custom data we want from the token.
-type CustomClaims struct {
-	Scope string `json:"scope"`
-}
+const AUTH0_AUDIENCE = "https://fs-apigw-bff-nakagome-bi5axj14.uc.gateway.dev/"
 
-// Validate does nothing for this example, but we need
-// it to satisfy validator.CustomClaims interface.
-func (c CustomClaims) Validate(ctx context.Context) error {
-	return nil
-}
+// // CustomClaims contains custom data we want from the token.
+// type CustomClaims struct {
+// 	Scope string `json:"scope"`
+// }
 
-// HasScope checks whether our claims have a specific scope.
-func (c CustomClaims) HasScope(expectedScope string) bool {
-	result := strings.Split(c.Scope, " ")
-	for i := range result {
-		if result[i] == expectedScope {
-			return true
-		}
-	}
+// // Validate does nothing for this example, but we need
+// // it to satisfy validator.CustomClaims interface.
+// func (c CustomClaims) Validate(ctx context.Context) error {
+// 	return nil
+// }
 
-	return false
-}
+// scopeは今回使用しない
+// // HasScope checks whether our claims have a specific scope.
+// func (c CustomClaims) HasScope(expectedScope string) bool {
+// 	result := strings.Split(c.Scope, " ")
+// 	for i := range result {
+// 		if result[i] == expectedScope {
+// 			return true
+// 		}
+// 	}
+
+// 	return false
+// }
 
 // EnsureValidToken is a middleware that will check the validity of our JWT.
 func EnsureValidToken() func(next http.Handler) http.Handler {
@@ -51,8 +54,9 @@ func EnsureValidToken() func(next http.Handler) http.Handler {
 		provider.KeyFunc,
 		validator.RS256,
 		issuerURL.String(),
-		[]string{"https://fs-apigw-bff-nakagome-bi5axj14.uc.gateway.dev/"},
+		[]string{AUTH0_AUDIENCE},
 		// []string{os.Getenv("AUTH0_AUDIENCE")},
+		// 今回はカスタムクレーム特にみない
 		// validator.WithCustomClaims(
 		// 	func() validator.CustomClaims {
 		// 		return &CustomClaims{}
