ffa59ebf05d8f5e2032c0c58f19dd1b9588e7ffe
scsk-nakagome <nakagome@scsk.jp>
2023-10-04 10:38:07 +0900

    test

---
+++ config.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"log"
+	"path"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed config/*.yaml
+var content embed.FS
+
+var RunningEnv string
+
+type Config struct {
+	Credentials struct {
+		// Password string `yaml:"password"`
+		PORT string `yaml:"port"`
+	}
+	Auth0 struct {
+		AUTH0_DOMAIN   string `yaml:"auth0Domain"`
+		AUTH0_AUDIENCE string `yaml:"auth0Audience"`
+	}
+}
+
+func (c *Config) GetConfig() *Config {
+
+	// 実行環境を取得
+	flag.StringVar(&RunningEnv, "runningEnv", "develop", "Environment to use")
+	// 実行環境の読み取り
+	flag.Parse()
+	log.Printf("runningEnv: %s", RunningEnv)
+	// 設定ファイル読み取り
+	content, err := content.Open(path.Join("config", fmt.Sprintf("%s.yaml", RunningEnv)))
+	if err != nil {
+		log.Fatalf("Failed to open content. err: %v", err)
+	}
+	config := &Config{}
+	decoder := yaml.NewDecoder(content)
+	if err := decoder.Decode(&config); err != nil {
+		log.Fatalf("Failed to decode yaml. err: %v", err)
+	}
+	log.Printf("Application setting file password: %v", config.Credentials)
+
+	return config
+}
--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"embed"
-	"flag"
-	"fmt"
+	// "embed"
+	// "flag"
+	// "fmt"
 	"log"
 	"net/http"
-	"path"
 
-	"gopkg.in/yaml.v3"
+	// "path"
+
+	// "gopkg.in/yaml.v3"
 
 	"github.com/GoogleCloudPlatform/golang-samples/run/helloworld/router"
 )
 
-//go:embed config/*.yaml
-var content embed.FS
 
-var runningEnv string
-
-type Config struct {
-	Credentials struct {
-		Password string `yaml:"password"`
-	}
-}
-
-func init() {
-	// 実行環境を取得
-	flag.StringVar(&runningEnv, "runningEnv", "qc", "Environment to use")
-}
 
 func main() {
-	// 実行環境の読み取り
-	flag.Parse()
-	log.Printf("runningEnv: %s", runningEnv)
-	// 設定ファイル読み取り
-	content, err := content.Open(path.Join("config", fmt.Sprintf("%s.yaml", runningEnv)))
-	if err != nil {
-		log.Fatalf("Failed to open content. err: %v", err)
-	}
-	config := &Config{}
-	decoder := yaml.NewDecoder(content)
-	if err := decoder.Decode(&config); err != nil {
-		log.Fatalf("Failed to decode yaml. err: %v", err)
-	}
-	log.Printf("Application setting file password: %s", config.Credentials.Password)
-
+	
 	rtr := router.New()
 	if err := http.ListenAndServe("0.0.0.0:8080", rtr); err != nil {
 		log.Fatalf("There was an error with the http server: %v", err)
