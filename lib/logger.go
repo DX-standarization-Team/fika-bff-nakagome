450311377c494f98c526e6a7ea8cf690c4e4051a
scsk-nakagome <nakagome@scsk.jp>
2023-12-19 17:55:38 +0900

    cloud logging

--- lib/logger.go
+++ lib/logger.go
@@ -9,6 +9,7 @@ import (
 
 func NewLogger() *logging.Logger {
 
+	log.Println("NewLogger entering")
 	ctx := context.Background()
 
 	// Sets your Google Cloud Platform project ID.
@@ -20,11 +21,13 @@ func NewLogger() *logging.Logger {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 	defer client.Close()
+	log.Printf("client: %v", client)
 
 	// Sets the name of the log to write to.
 	logName := "my-log"
 	// Selects the log to write to.
 	logger := client.Logger(logName)
+	log.Printf("logger: %v", logger)
 	defer logger.Flush()
 
 	return logger
